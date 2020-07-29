71a6adcdfb8cab61e763e088ab137a3cd20b752c
yuridido <yuridido@yahoo.it>
2020-07-29 16:34:38 +0200

    mess+risposta

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    var risp = [
+        "ok",
+        "ciao",
+        "saluti",
+        "ue"
+    ];
+
+
+
+
+
+
     // FACCIO PARTIRE IL MESSAGGIO CON IL TASTO INVIO
     $('#mess-text').keydown(function(event) {
         if (event.which == 13) {
@@ -28,29 +40,36 @@ $(document).ready(function() {
 
 
 
+    // FUNZIONI
+    // funzione per creare il mio messaggio
+    function invio() {
+        var testo = $('#mess-text').val();
+        var contenitore = $('.template .messaggio').clone().addClass('mandato');
+        var d = new Date();
+        var ora = d.getHours();
+        var minuti = d.getMinutes();
+        $(contenitore).children('p').append(testo);
+        $(contenitore).children('.ora-mess').append(ora + ':' + minuti);
+        $('.text-box').append(contenitore);
+        setTimeout(risposta, 2000);
+    }
+    //funzione per risposta automatica
+    function risposta() {
+
+        var contenitore = $('.template .messaggio').clone().addClass('ricevuto');
+        var d = new Date();
+        var ora = d.getHours();
+        var minuti = d.getMinutes();
+        $(contenitore).children('p').append(risp[random(0, 3)]);
+        $(contenitore).children('.ora-mess').append(ora + ':' + minuti);
+        $('.text-box').append(contenitore);
+    }
+
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
 
 
 
 });
-
-
-
-
-
-
-
-
-// FUNZIONI
-// funzione per creare il mio messaggio
-function invio() {
-    var testo = $('#mess-text').val();
-    var contenitore = $('.template .messaggio').clone().addClass('mandato');
-    var d = new Date();
-    var ora = d.getHours();
-    var minuti = d.getMinutes();
-    $(contenitore).children('p').append(testo);
-    $(contenitore).children('.ora-mess').append(ora + ':' + minuti);
-    $('.text-box').append(contenitore);
-
-}
